96539d5ddb63aee9cda330f9837d973de53704ed
do5do <do151015@gmail.com>
2023-11-06 19:47:13 +0900

    properties 분리

--- consumer/src/main/kotlin/com/zerobase/consumer/service/LoanRequestService.kt
+++ consumer/src/main/kotlin/com/zerobase/consumer/service/LoanRequestService.kt
@@ -4,19 +4,18 @@ import com.zerobase.consumer.dto.ReviewResponseDto
 import com.zerobase.domain.domain.LoanReview
 import com.zerobase.domain.repository.LoanReviewRepository
 import com.zerobase.kafka.dto.LoanRequestDto
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.stereotype.Service
 import java.time.Duration
 
 @Service
 class LoanRequestService(
-    private val loanReviewRepository: LoanReviewRepository
+    private val loanReviewRepository: LoanReviewRepository,
+
+    @Value("\${css.url}")
+    private val url: String
 ) {
-    companion object {
-        // proxy server(nginx)를 통해서 css api를 호출하기 때문에 nginx로 보낸다.
-//        const val nginxUrl = "http://nginx:8085/css/api/v1/request"
-        const val nginxUrl = "http://localhost:8086/css/api/v1/request"
-    }
 
     fun loanRequest(loanRequestDto: LoanRequestDto) {
         val reviewResult = loanRequestToCb(loanRequestDto)
@@ -30,7 +29,7 @@ class LoanRequestService(
             .setReadTimeout(Duration.ofMillis(1000))
             .build()
 
-        return restTemplate.postForEntity(nginxUrl, loanRequestDto, ReviewResponseDto::class.java).body!!
+        return restTemplate.postForEntity(url, loanRequestDto, ReviewResponseDto::class.java).body!!
     }
 
     fun saveLoanReviewData(loanReview: LoanReview) = loanReviewRepository.save(loanReview)
